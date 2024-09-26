db6cf9370173de6c550b93f725d64569eec5afae
petrsovadina <petr.sovadina9@gmail.com>
2024-09-26 14:09:39 +0200

    add forms

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['ghost-dso8k808400okgkc80wss8s0.digimedic.cz', 'www.gravatar.com'],
+  },
+}
+
+module.exports = nextConfig
