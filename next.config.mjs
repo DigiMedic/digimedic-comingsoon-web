9a75287d60d40d0aaa5dd46cbef189458ffd4508
petrsovadina <petr.sovadina9@gmail.com>
2024-10-17 14:54:34 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -8,36 +8,6 @@ const __dirname = dirname(__filename);
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    // Zde by nemělo být nic, pokud nepoužíváte jiné experimentální funkce
-  },
-  images: {
-    domains: [
-      'ghost-dso8k808400okgkc80wss8s0.digimedic.cz',
-      'digimedic.cz',
-      'www.digimedic.cz',
-      'utfs.io',
-      'ghost-dso8k808400okgkc80wss8s0.194.164.72.131.sslip.io'
-    ],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.digimedic.cz',
-      },
-      {
-        protocol: 'https',
-        hostname: 'utfs.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ghost-dso8k808400okgkc80wss8s0.194.164.72.131.sslip.io',
-      },
-    ],
-  },
-  env: {
-    GHOST_API_URL: process.env.GHOST_API_URL,
-    GHOST_CONTENT_API_KEY: process.env.GHOST_CONTENT_API_KEY,
-  },
 };
 
 export default nextConfig;
