e32c68f84578e7476bb938872f490326559d785c
petrsovadina <petr.sovadina9@gmail.com>
2024-10-17 15:28:16 +0200

    fix

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          defer
+          src="http://umami-zgwccgo44s8g04840wscokg8.194.164.72.131.sslip.io/script.js"
+          data-website-id="fdf06559-4cb9-46b4-b35b-758e035aa583"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
