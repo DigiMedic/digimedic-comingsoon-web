7bedee18f1526ed27732d450b15cb4395b885fb6
petrsovadina <petr.sovadina9@gmail.com>
2024-12-26 01:31:06 +0100

    ok

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -3,10 +3,17 @@
 import Script from "next/script"
 
 export default function Analytics() {
+  const UMAMI_URL = process.env.NEXT_PUBLIC_UMAMI_URL || 'https://umami.digimedic.dev'
+  
+  if (!UMAMI_URL) {
+    console.warn('NEXT_PUBLIC_UMAMI_URL není nastaveno')
+    return null
+  }
+
   return (
     <Script
       defer
-      src="http://umami-zgwccgo44s8g04840wscokg8.194.164.72.131.sslip.io/script.js"
+      src={`${UMAMI_URL}/script.js`}
       data-website-id="fdf06559-4cb9-46b4-b35b-758e035aa583"
       strategy="afterInteractive"
     />
